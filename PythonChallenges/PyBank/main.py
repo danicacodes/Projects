d84029abebaf676434bf4132d602d8bc833c7044
Rios <dnunez@richmond.edu>
2019-05-07 14:51:24 -0400

    Added number of months

--- PythonChallenges/PyBank/main.py
+++ PythonChallenges/PyBank/main.py
@@ -17,34 +17,30 @@ import csv
 
 csvpath = os.path.join(".","python-challenge","PythonChallenges","PyBank",'budget_data.csv')
 
-# # Method 1: Plain Reading of CSV files
-# with open(csvpath, 'r') as file_handler:
-#     lines = file_handler.read()
-#     print(lines)
-#     print(type(lines))
-
-
 # Method 2: Improved Reading using CSV module
-
 with open(csvpath, newline='') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-
+    
+    # Go through file and add all the numbers in the second column, which is Net Profit and Losses of each month
     csv_header = next(csvreader)
     total = 0
     for row in csv.reader(csvfile):
         total += int(row[1])
-    #print(total)
-    #print(csvreader)
-    # Read the header row first
-    #print(f"CSV Header: {csv_header}")
 
+with open(csvpath, newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    data = list(reader)
+    row_count = len(data)-1    
+
+    
 # -----------------------------------------------------------------------
 # Print Summary
 print("Financial Analysis")
 print("------------------------")
+print(f"Total Months: {str(row_count)}")
 print(f"Total: ${str(total)}")
-print("Average Change: ")
-print("Greatest Increase in Profits: ")
-print("Greatest Decrease in Profits: ")
+print("Average Change: $")
+print(f"Greatest Increase in Profits: $")
+print("Greatest Decrease in Profits: $")
