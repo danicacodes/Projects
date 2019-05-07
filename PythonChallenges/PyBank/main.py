a589e6a43ae84cb5c7ab8b7ef4ae1a0505f408ab
Rios <dnunez@richmond.edu>
2019-05-07 16:05:00 -0400

    Almost done

--- PythonChallenges/PyBank/main.py
+++ PythonChallenges/PyBank/main.py
@@ -17,30 +17,36 @@ import csv
 
 csvpath = os.path.join(".","python-challenge","PythonChallenges","PyBank",'budget_data.csv')
 
+# Create lists
+total_months = []
+total_profit = []
+monthly_change = []
+
 # Method 2: Improved Reading using CSV module
 with open(csvpath, newline='') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
     
-    # Go through file and add all the numbers in the second column, which is Net Profit and Losses of each month
+    # Go through file and add all the numbers in the second column to get Total Net Profit for entire data set
     csv_header = next(csvreader)
-    total = 0
     for row in csv.reader(csvfile):
-        total += int(row[1])
+        total_months.append(row[0]) 
+        total_profit.append(int(row[1]))
 
-with open(csvpath, newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    data = list(reader)
-    row_count = len(data)-1    
-
+    # Iterate through all profits by month to get the changes
+    for i in range(len(total_profit)-1):
+        monthly_change.append(total_profit[i+1]-total_profit[i])
     
+    # In the monthly change list, find max and min of monthly change value
+    max_increase_change = max(monthly_change)
+    max_decrease_change = min(monthly_change)
 # -----------------------------------------------------------------------
-# Print Summary
+# Print Financial Analysis Summary
 print("Financial Analysis")
 print("------------------------")
-print(f"Total Months: {str(row_count)}")
-print(f"Total: ${str(total)}")
-print("Average Change: $")
-print(f"Greatest Increase in Profits: $")
-print("Greatest Decrease in Profits: $")
+print(f"Total Months: {len(total_months)}")
+print(f"Total: ${sum(total_profit)}")
+print(f"Average Change: ${round(sum(monthly_change)/len(monthly_change),2)}")
+print(f"Greatest Increase in Profits: ${(str(max_increase_change))}")
+print(f"Greatest Decrease in Profits: ${(str(max_decrease_change))}")
