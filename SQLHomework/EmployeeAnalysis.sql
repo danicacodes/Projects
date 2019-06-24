53e0e075f044445d3d607b1406bc8152dceb94e1
Rios <dnunez@richmond.edu>
2019-06-24 18:34:41 -0400

    Added Tables

--- SQLHomework/EmployeeAnalysis.sql
+++ SQLHomework/EmployeeAnalysis.sql
@@ -44,3 +44,43 @@ CREATE TABLE "dept_emp" (
 
 SELECT * FROM dept_emp;
 
+CREATE TABLE "dept_manager" (
+	"dept_no" VARCHAR(10) NOT NULL,
+	"emp_no" INT NOT NULL,
+	"from_date" DATE NOT NULL,
+	"to_date" DATE NOT NULL
+);
+
+SELECT * FROM dept_manager;
+
+CREATE TABLE "employees" (
+	"emp_no" INT NOT NULL,
+	"birth_date" DATE NOT NULL,
+	"first_name" VARCHAR NOT NULL,
+	"last_name" VARCHAR NOT NULL,
+	"gender" VARCHAR NOT NULL,
+	"hire_date" DATE NOT NULL
+);
+
+SELECT * FROM employees;
+
+CREATE TABLE "salaries" (
+	"emp_no" INT NOT NULL,
+	"salary" INT NOT NULL,
+	"from_date" DATE NOT NULL,
+	"to_date" DATE NOT NULL
+);
+
+SELECT * FROM salaries;
+
+CREATE TABLE "titles" (
+	"emp_no" INT NOT NULL,
+	"title" VARCHAR NOT NULL,
+	"from_date" DATE NOT NULL,
+	"to_date" DATE NOT NULL
+);
+
+SELECT * FROM titles;
+
+
+
