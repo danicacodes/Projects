d5694f6b61ca350e3ca30df8bb7cc0af6ff49c1e
Rios <dnunez@richmond.edu>
2019-06-25 17:29:35 -0400

    Added Queries 1 thru 3 for Data Analysis

--- SQLHomework/dataanalysis.sql
+++ SQLHomework/dataanalysis.sql
@@ -17,12 +17,39 @@ COMMENT ON DATABASE "Employee80s90s_db"
 	--Data Analysis--
 	-- 1. List the following details of each employee: employee number, last name, first name, gender, and salary
 	
-	SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
+	SELECT e.emp_no AS Employee_Number, e.last_name AS Last_Name, e.first_name AS First_Name, e.gender AS Gender, 
+	s.salary AS Salary
 	FROM employees e
 	JOIN salaries s
 	ON e.emp_no = s.emp_no;
 	
 	-- 2. List employees who were hired in 1986
+	
+	SELECT * 
+	FROM employees e
+	WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
+	
+	-- 3. List the manager of each department with the following information: department number,
+	-- department name, the manager's employee number, last name, first name, and start and end employment dates.
+	SELECT d.dept_no AS Department_Number, d.dept_name AS Department_Name, 
+	m.emp_no AS Managers_Employee_Number, e.last_name AS Last_Name, e.first_name AS First_Name, 
+	m.from_date AS Start_Employment_Dates, m.to_date AS End_Employment_Dates  
+	FROM departments d
+	JOIN dept_manager m
+	ON (d.dept_no = m.dept_no)
+		JOIN employees e
+		ON (m.emp_no = e.emp_no);
+		
+	-- 4. List the department of each employee with the following information: employee number, last name, first name
+	-- and, department name.
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
