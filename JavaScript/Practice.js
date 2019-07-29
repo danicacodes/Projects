b092729357820b5b0738cbea7fb7eb86824972e9
Rios <dnunez@richmond.edu>
2019-07-29 17:48:28 -0400

    Changed name of folder

---
+++ JavaScript/Practice.js
@@ -0,0 +1,42 @@
+
+// This code will run when linked to in HTML
+// This is a print statement in JavaScript and printing it in the console, which is in the developer tools
+// Semi-colons aren't always required but you should always use them to get into the habit
+// Need to put it into HTML document
+console.log("My script is stored outside of the HTML!");
+console.log("I love learning new programming languages")
+
+// Create a variable called "name" that holds a string
+var name = "Homer Simpson";
+
+// Create a variable called "country" that holds a string
+var country = "United States";
+
+// Create a variable called "age" that holds an integer
+var age = 26;
+
+// Create a variable called "hourlyWage" that holds an integer
+var hourlyWage = 15;
+
+// Calculate the "dailyWage" for the user
+var dailyWage = hourlyWage * 8;
+
+// Create a variable that holds a number as a string
+var weeklyHours = "40";
+
+// Create a variable called 'weeklyWage' that converts a string into an integer
+var weeklyWage = hourlyWage * parseInt(weeklyHours);
+
+//Create a variable called satisfied that holds a boolean
+var satisfied = true;
+
+// Print out "Hello <name>!"
+console.log(`Hello  + ${name}!`);
+
+// Creates an alert box
+alert(`Hello  + ${name}!`);
+
+// Creates text in body
+document.body.innerHTML = `<h1>The date today is “ + date + “</h1>`;
+
+
