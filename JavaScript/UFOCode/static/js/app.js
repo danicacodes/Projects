706d537768173c3863bbc388f462e648ead2a942
Rios <dnunez@richmond.edu>
2019-08-03 15:06:39 -0400

    Added Filters

--- JavaScript/UFOCode/static/js/app.js
+++ JavaScript/UFOCode/static/js/app.js
@@ -1,53 +1,54 @@
+// Declare Variables
+// Need to add ids to Index
+// this is under a form
+var datetime = d3.select('#date');
+var city = d3.select('#city');
+var state = d3.select('#state');
+var country = d3.select('#country');
+var shape = d3.select('#shape');
+var duration = d3.select('#duration');
+var comments = d3.select('#comments');
+var button = d3.select('#filter-btn');
+
 // from data.js
 var tableData = data;
 var tbody = d3.select("tbody"); //tbody tag
 
-data.forEach((ufo_sightings) => {
-    //console.log(ufo_sightings);
-    var row = tbody.append('tr'); //appending tr tag that doesn't exist so it is creating one
-    Object.entries(ufo_sightings).forEach(([key, value]) => {
-        //console.log(key, value);
-        var cell = row.append('td');
-        cell.text(value);
+function init(){ 
+    data.forEach((ufo_sightings) => {
+        console.log(ufo_sightings);
+        var row = tbody.append('tr'); //appending tr tag that doesn't exist so it is creating one
+        Object.entries(ufo_sightings).forEach(([key, value]) => {
+            console.log(key, value);
+            var cell = row.append('td');
+            cell.text(value);
+        });
     });
-});
+}
 
-//console.log(data);
+init();
 
+// YOUR CODE HERE!
+button.on("click", function() {
+    console.log("Button Clicked!");
+    tbody.html("");
 
-// var columns = ['datetime', 'city', 'state', 'country', 'shape', 'durationMinutes', 'comments']
+    var newdate = d3.select('#datetime').property("value");
+    console.log(newdate);
 
-// // ufo_sightings scope is within this function
-// data.forEach(function(ufo_sightings){
-//     console.log(ufo_sightings)
-// });
-
-// // Append the data. Is this extra???
-// data.forEach(function(ufo_sightings) {
-//     console.log();
-//     var row = tbody.append('tr');
-// })
-
-
-// // var populate = (dataInput) => {
-// //     dataInput.forEach(function(ufo_sightings) => {
-// //         var row = d3.select('tbody').append('tr');
-// //         columns.forEach(column => row.append('td').text(ufo_sightings[column]))
-// //     });
-// // }
-
-// // YOUR CODE HERE!
-// button.on("click", function() {
-//     tbody.html("");
-
-// function buildTable(data){
-//     tbody.html("");
-//     data.forEach((dataRow) => {
-//         let row = tbody.append('tr');
-//         Object.values(datarow).forEach((val) => {
-//             let cell = row.append('td');
-//             cell.text(val);
-//         });
-//     })
-// }
-
+    var outputData = data.filter(function(filterData) {
+        return(filterData.datetime == newdate)
+    });
+    
+        console.log(outputData);
+        outputData.forEach((ufo_sightings) => {
+            console.log(ufo_sightings);
+            var row = tbody.append('tr'); //appending tr tag that doesn't exist so it is creating one
+            Object.entries(ufo_sightings).forEach(([key, value]) => {
+                console.log(key, value);
+                var cell = row.append('td');
+                cell.text(value);
+    
+            });
+        });
+    });
